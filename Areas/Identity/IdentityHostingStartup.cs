af56e168cef762a511b1a3ef182291c4b79106ed
AlenHristov <alen.hristov@abv.bg>
2021-01-31 17:39:00 +0200

    Identity

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MusicMVC.Data;
+
+[assembly: HostingStartup(typeof(MusicMVC.Areas.Identity.IdentityHostingStartup))]
+namespace MusicMVC.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
