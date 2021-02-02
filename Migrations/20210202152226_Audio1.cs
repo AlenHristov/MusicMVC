525527d8f8564bc765d07cd2caab740dd23b6745
AlenHristov <alen.hristov@abv.bg>
2021-02-02 18:16:57 +0200

    Removed ConfirmEmail

---
+++ Migrations/20210202152226_Audio1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MusicMVC.Migrations
+{
+    public partial class Audio1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
